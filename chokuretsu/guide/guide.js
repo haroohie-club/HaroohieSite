2908e808b337322e48ad5e433b73a3350ef2d5df
William <will27528@gmail.com>
2022-02-27 21:39:35 +0000

    choose platform and intro page work

--- chokuretsu/guide/guide.js
+++ chokuretsu/guide/guide.js
@@ -2,8 +2,17 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
-
 let device = params.device;
-if (device != null) {
-    alert(device);
-}
+
+// Ensures device param carries over between browsing guide pages
+window.addEventListener('load', function(event){
+    if (device != null) {
+        let elements = document.getElementsByTagName('a');
+        for(let i = 0; i < elements.length; i++) {
+            if (elements[i].classList.contains('exclude-device-href')) {
+                continue; // Exclude certain links from this
+            }
+            elements[i].href += '?device=' + device;
+        }
+    }
+})
