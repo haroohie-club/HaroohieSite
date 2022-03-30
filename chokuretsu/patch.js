cac12fbec666ca03180e7248564e7579122a600c
William <will27528@gmail.com>
2022-03-30 01:48:49 +0100

    Get new headless patcher system working

--- chokuretsu/patch.js
+++ chokuretsu/patch.js
@@ -1,90 +1,180 @@
-// Fetches the latest releases from the releases repository and presents them in a nice dropdown
-import {request} from "https://cdn.skypack.dev/@octokit/request";
-
+// Patcher settings
 const REPO_ORG = 'haroohie-club';
 const REPO = 'ChokuretsuTranslationRelease';
-let hasFetched = false;
+const CORS_PROXY = 'https://cors.haroohie.club/';
 
-// Fetch releases from GitHub API
-request('GET /repos/{owner}/{repo}/releases', {
-    owner: REPO_ORG,
-    repo: REPO
-}).then(releaseList => {
-    if (releaseList.data.length === 0) {
-        defaultToManualUpload('There are no releases yet! Come back soon!')
-        return;
+// Debug flags
+const DEBUG_MODE = true;
+const DEBUG_PATCH = '';
+
+// RomPatcher data variables
+let romFile, patchFile, patch, romFile1, romFile2, tempFile, headerSize, oldHeader;
+
+// Run when the window loads
+window.onload = () => {
+    let script = document.createElement('script');
+    script.src = '../patcher/js/zip.js/inflate.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+    // When a ROM file is selected
+    document.getElementById('input-file-rom').addEventListener('change', function () {
+        romFile = new MarcFile(this, _parseROM);
+
+        let patchButton = document.getElementById('patcher-patch-button');
+        patchButton.classList.remove('disabled');
+        patchButton.disabled = false;
+    });
+
+    // When the Patch ROM button is pressed
+    document.getElementById('patcher-patch-button').addEventListener("click", function () {
+
+        let version = document.getElementById('patcher-version-dropdown').value;
+
+        // Gets the file name
+        parsePatchFile(getFileName(), version).then(arrayBuffer => {
+            console.log(arrayBuffer);
+            return new MarcFile(arrayBuffer);
+        }).then(parsedFile => {
+            patchFile = parsedFile;
+            patchFile.littleEndian = false;
+            patchFile.fileName = getFileName();
+
+            let header = patchFile.readString(6);
+            if (header.startsWith(ZIP_MAGIC)) {
+                patch = false;
+                ZIPManager.parseFile(patchFile);
+            } else {
+                patch = parseVCDIFF(patchFile);
+            }
+        }).then(() => {
+            applyPatch(patch, romFile, false);
+        });
+    });
+}
+
+function _parseROM() {
+    if (romFile.readString(4).startsWith(ZIP_MAGIC)) {
+        ZIPManager.parseFile(romFile);
+    } else {
+        if ((headerSize = canHaveFakeHeader(romFile))) {
+        } else if ((headerSize = hasHeader(romFile))) {
+        }
     }
+}
 
-    const select = document.createElement('select');
-    select.disabled = true;
-    select.id = 'input-file-patch';
-    el('input-file-patch').parentElement.replaceChild(select, el('input-file-patch'));
-    select.parentElement.title = '';
 
-    // Iterate through releases and sort by date
-    releaseList.data.forEach(release => {
-        request('GET /repos/{owner}/{repo}/releases/{release_id}/assets', {
-            owner: REPO_ORG,
-            repo: REPO,
-            release_id: release.id
-        }).then(releaseAssetList => {
-            releaseAssetList.data.sort(function (a, b) {
-                return new Date(b.created_at) - new Date(a.created_at);
-            }).forEach(releaseAsset => {
-                let downloadUrl = CORS_PROXY + releaseAsset.browser_download_url;
-                if (downloadUrl.endsWith('.xdelta')) {
-                    let date = new Date(release.published_at);
-                    let dateString = date.toISOString().slice(0, 10); // Format date as YYYY-MM-DD
+// Gets the name of the file needed to be fetched to patch
+function getFileName() {
+    let opEdSubsConfig = document.querySelector('input[name="op-ed-subtitling"]:checked').value;
+    let voicedLineConfig = document.querySelector('input[name="voice-lines-subtitling"]:checked').value;
 
-                    let nameString = 'Version ' + release.tag_name + ' (' + dateString + ')';
-                    if (CUSTOM_PATCHER.length === 0) {
-                        nameString += ' (Latest)';
-                    }
+    // Possible file names: patch-(subbedoped|cleanoped)-(voicesubs|novoicesubs).xdelta
+    return ('patch-' + opEdSubsConfig + '-' + voicedLineConfig + '.xdelta');
+}
 
-                    let i = CUSTOM_PATCHER.push({
-                        file: downloadUrl,
-                        name: nameString,
-                    }) - 1;
+// Returns the versioned patch file with the given name from the GitHub org
+function parsePatchFile(fileName, version) {
+    // Download from URL
+    showNotice('info', 'Downloading patch...');
 
-                    CUSTOM_PATCHER[i].fetchedFile = false;
+    let encodedUri;
+    if (!DEBUG_MODE) {
+        encodedUri = (CORS_PROXY + 'https://github.com/' + REPO_ORG + '/' + REPO + '/releases/' + version + '/download/' + fileName);
+    } else {
+        encodedUri = DEBUG_PATCH;
+    }
+    let fileUri = decodeURI(encodedUri.trim());
+    return fetch(fileUri).then(result => result.arrayBuffer()) // Gets the response and returns it as a blob
+        .then(arrayBuffer => {
+            return arrayBuffer;
+        }).catch(function (error) {
+            console.error(error);
+            showNotice('error', 'An error occurred downloading the patch file from GitHub: ' + error.message)
+            return undefined;
+        });
+}
 
-                    CUSTOM_PATCHER[i].selectOption = document.createElement('option');
-                    CUSTOM_PATCHER[i].selectOption.value = i;
-                    CUSTOM_PATCHER[i].selectOption.innerHTML = CUSTOM_PATCHER[i].name || CUSTOM_PATCHER[i].file;
+function applyPatch(patch, rom, validateChecksums) {
+    console.log(patch);
+    console.log(rom);
+    if (patch && rom) {
+        showNotice('info', 'Applying patch...');
 
-                    select.appendChild(CUSTOM_PATCHER[i].selectOption);
+        // Patch the rom
+        try {
+            patch.apply(rom, validateChecksums);
+            preparePatchedRom(rom, patch.apply(rom, validateChecksums));
+        } catch (error) {
+            console.error(error);
+            showNotice('error', 'Error applying patch: ' + error.message);
+        }
 
-                    if (typeof CUSTOM_PATCHER[i].patches === 'object') {
-                        for (let j = 0; j < CUSTOM_PATCHER[i].patches.length; j++) {
-                            if (j === 0) {
-                                CUSTOM_PATCHER[i].patches[0].selectOption = CUSTOM_PATCHER[i].selectOption;
-                                CUSTOM_PATCHER[i].selectOption = null;
-                            } else {
-                                CUSTOM_PATCHER[i].patches[j].selectOption = document.createElement('option');
-                                select.appendChild(CUSTOM_PATCHER[i].patches[j].selectOption);
-                            }
+    } else {
+        if (patch === undefined) {
+            showNotice('error', 'Failed to retrieve the patch file.');
+        } else {
+            showNotice('error', 'Please choose a ROM first');
+        }
+    }
+}
 
-                            CUSTOM_PATCHER[i].patches[j].selectOption.value = i + ',' + j;
-                            CUSTOM_PATCHER[i].patches[j].selectOption.innerHTML = CUSTOM_PATCHER[i].patches[j].name || CUSTOM_PATCHER[i].patches[j].file;
-                        }
-                    }
+// Prepare the final patched ROM
+function preparePatchedRom(originalRom, patchedRom) {
+    patchedRom.fileName = originalRom.fileName.replace(/\.([^\\.]*?)$/, ' (patched).$1');
+    patchedRom.fileType = originalRom.fileType;
+    patchedRom.save();
+    hideNotice();
+}
 
-                    if (!hasFetched) {
-                        fetchPatch(i, 0, request);
-                        hasFetched = true;
-                    }
-                }
-            })
-        })
-    })
+function canHaveFakeHeader(romFile) {
+    if (romFile.fileSize <= 0x600000) {
+        for (let i = 0; i < HEADERS_INFO.length; i++) {
+            if (HEADERS_INFO[i][0].test(romFile.fileName) && (romFile.fileSize % HEADERS_INFO[i][2] === 0)) {
+                return HEADERS_INFO[i][1];
+            }
+        }
+    }
+    return 0;
+}
 
-    // Prepare patcher
-    preparePatcher(request);
-}).catch(() => {
-    defaultToManualUpload('This page is currently experiencing heavy load; could not automatically download the latest patch.<br/>Please download the latest .xdelta patch file from the <a href="https://github.com/haroohie-club/ChokuretsuTranslationRelease" target="_blank"><i class="fa-brands fa-github"></i> GitHub releases repository</a> and select it in the \'Select patch file\' box, then select your ROM and apply the patch.')
-});
+function hasHeader(romFile) {
+    if (romFile.fileSize <= 0x600200) {
+        if (romFile.fileSize % 1024 === 0)
+            return 0;
 
-function defaultToManualUpload(error) {
-    document.getElementById('select-patch-version').innerHTML = 'Select patch file';
-    document.getElementById('message-apply').innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i>&nbsp;' + error;
+        for (let i = 0; i < HEADERS_INFO.length; i++) {
+            if (HEADERS_INFO[i][0].test(romFile.fileName) && (romFile.fileSize - HEADERS_INFO[i][1]) % HEADERS_INFO[i][1] === 0) {
+                return HEADERS_INFO[i][1];
+            }
+        }
+    }
+    return 0;
+}
+
+// Show the patcher status notice at the top of the patcher
+function showNotice(noticeType, noticeMessage) {
+    let messageBody = '';
+    switch (noticeType) {
+        case 'error':
+            messageBody += '<i class="fa-solid fa-circle-xmark"></i>&nbsp;'
+            break;
+        case 'warning':
+            messageBody += '<i class="fa-solid fa-triangle-exclamation"></i>&nbsp;'
+            break;
+        default:
+            messageBody += '<i class="fa-solid fa-circle-info"></i>&nbsp;'
+            break;
+    }
+    messageBody += noticeMessage;
+    let patcherElement = document.getElementById('patcher-notice');
+    patcherElement.innerHTML = messageBody;
+    patcherElement.classList.remove('hidden');
+}
+
+// Hide the patcher status notice
+function hideNotice() {
+    let patcherElement = document.getElementById('patcher-notice');
+    if (!patcherElement.classList.contains('hidden')) {
+        patcherElement.classList.add('hidden');
+    }
 }
