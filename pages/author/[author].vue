410e45958355a1cc1073eaf7ca69d5484133557e
William <will27528@gmail.com>
2022-10-16 02:04:25 +0100

    Add error handling for author and blogs

--- pages/author/[author].vue
+++ pages/author/[author].vue
@@ -1,23 +1,29 @@
 <template>
     <NuxtLayout>
         <div class="box" id="container">
-            <ContentDoc v-slot="{ doc }">
-                <div v-if="doc.author">
-                    <h1 id="author">{{ doc.author.name }}</h1>
-                    <div id="social">
-                        <NuxtLink v-if="doc.author.twitter" :to="doc.author.twitter">
-                            <IconifiedText icon="fa6-brands:twitter">Twitter</IconifiedText>
-                        </NuxtLink>
-                        <NuxtLink v-if="doc.author.github" :to="doc.author.github">
-                            <IconifiedText icon="fa6-brands:github">GitHub</IconifiedText>
-                        </NuxtLink>
+            <ContentDoc>
+                <template v-slot="{ doc }">
+                    <div v-if="doc.author">
+                        <h1 id="author">{{ doc.author.name }}</h1>
+                        <div id="social">
+                            <NuxtLink v-if="doc.author.twitter" :to="doc.author.twitter">
+                                <IconifiedText icon="fa6-brands:twitter">Twitter</IconifiedText>
+                            </NuxtLink>
+                            <NuxtLink v-if="doc.author.github" :to="doc.author.github">
+                                <IconifiedText icon="fa6-brands:github">GitHub</IconifiedText>
+                            </NuxtLink>
+                        </div>
                     </div>
-                </div>
-                <ContentRenderer :value="doc" />
-                <h2>Latest posts by {{ doc.author.name }}</h2>
-                <BlogAuthorStack :author="doc.author.name" />
-                <ButtonLink link="/" color="red" icon="fa6-solid:house">Back to Home</ButtonLink>
+                    <ContentRenderer :value="doc" />
+                    <h2>Latest posts by {{ doc.author.name }}</h2>
+                    <BlogAuthorStack :author="doc.author.name" />
+                </template>
+                <template #not-found>
+                    <h1 id="author">Author not found</h1>
+                    <p>Could not find information for this author.</p>
+                </template>
             </ContentDoc>
+            <ButtonLink link="/" color="red" icon="fa6-solid:house">Back to Home</ButtonLink>
         </div>
     </NuxtLayout>
 </template>
--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -14,32 +14,40 @@
             <div class="blog">
                 <div class="content box">
                     <article>
-                        <ContentDoc v-slot="{ doc }">
-                            <div class="header" v-if="doc.navigation">
-                                <div class="brigade-logo">
-                                    <img src="/images/sos-logo.png" />
-                                </div>
-                                <div class="info">
-                                    <h1>{{ doc.navigation.title }}</h1>
-                                    <div class="meta">
-                                        <div class="date">
-                                            <IconifiedText icon="fa6-solid:newspaper">
-                                                Written by <NuxtLink
-                                                    :to="'/author/' + doc.navigation.author.toLowerCase()">{{
-                                                    doc.navigation.author }}</NuxtLink> on {{ doc.navigation.publishedAt
-                                                    }}
-                                            </IconifiedText>
-                                        </div>
-                                        <div class="tags">
-                                            <IconifiedText icon="fa6-solid:tags">Tags:</IconifiedText>
-                                            <span class="tag" v-for="tag of doc.navigation.tags">
-                                                <NuxtLink :to="'/tag/' + tag">{{ tag }}</NuxtLink>
-                                            </span>
+                        <ContentDoc>
+                            <template v-slot="{ doc }">
+                                <div class="header" v-if="doc.navigation">
+                                    <div class="brigade-logo">
+                                        <img src="/images/sos-logo.png" />
+                                    </div>
+                                    <div class="info">
+                                        <h1>{{ doc.navigation.title }}</h1>
+                                        <div class="meta">
+                                            <div class="date">
+                                                <IconifiedText icon="fa6-solid:newspaper">
+                                                    Written by <NuxtLink
+                                                        :to="'/author/' + doc.navigation.author.toLowerCase()">{{
+                                                        doc.navigation.author }}</NuxtLink> on {{
+                                                        doc.navigation.publishedAt
+                                                        }}
+                                                </IconifiedText>
+                                            </div>
+                                            <div class="tags">
+                                                <IconifiedText icon="fa6-solid:tags">Tags:</IconifiedText>
+                                                <span class="tag" v-for="tag of doc.navigation.tags">
+                                                    <NuxtLink :to="'/tag/' + tag">{{ tag }}</NuxtLink>
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <ContentRenderer :value="doc" />
+                                <ContentRenderer :value="doc" />
+                            </template>
+                            <template #not-found>
+                                <h1>Post not found</h1>
+                                <p>Could not find a blog post at this address.</p>
+                                <ButtonLink link="/blog" color="red" icon="fa6-solid:paper-plane">Back to the Blog</ButtonLink>
+                            </template>
                         </ContentDoc>
                     </article>
                 </div>
