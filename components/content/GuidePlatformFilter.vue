402269c9dba4adeefb31e79f6376c669fc998aba
William <will27528@gmail.com>
2022-10-16 19:15:43 +0100

    Add platform filters to be used on pages

---
+++ components/content/GuidePlatformFilter.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="filter-buttons">
+        <div class="platforms-label">
+            <IconifiedText icon="fa6-solid:gamepad"><b>Select: </b></IconifiedText>
+        </div>
+        <div class="platform-filters">
+            <div v-for="platform of filters" class="filter deselected"
+                :id="'filter-' + platform.toLowerCase().split(' ').join('_')" @click="clickFilter(platform)">
+                {{ platform }}
+            </div>
+        </div>
+    </div>
+
+    <div class="filtered-content">
+        <slot />
+    </div>
+</template>
+
+<script>
+import IconifiedText from './IconifiedText.vue'
+const selectedFilters = []
+
+export default {
+    props: {
+        filters: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        clickFilter: function (filter) {
+            if (selectedFilters.includes(filter)) {
+                selectedFilters.splice(selectedFilters.indexOf(filter), 1);
+                document.getElementById('filter-' + filter.toLowerCase().split(' ').join('_')).classList.add('deselected');
+                
+                // Get everything that has the filter class
+                const filtered = document.getElementsByClassName('platform-' + filter.toLowerCase().split(' ').join('_'));
+                for (let i = 0; i < filtered.length; i++) {
+                    filtered[i].classList.add('platform-filtered');
+                }
+            }
+            else {
+                selectedFilters.push(filter);
+                document.getElementById('filter-' + filter.toLowerCase().split(' ').join('_')).classList.remove('deselected');
+                
+                // Get everything that has the filter class
+                const filtered = document.getElementsByClassName('platform-' + filter.toLowerCase().split(' ').join('_'));
+                for (let i = 0; i < filtered.length; i++) {
+                    filtered[i].classList.remove('platform-filtered');
+                }
+            }
+            this.$emit("filter", selectedFilters);
+        }
+    },
+    components: { IconifiedText }
+}
+</script>
+
+<style scoped>
+.filter-buttons {
+    display: flex;
+    flex-direction: row;
+    font-size: 1.1rem;
+    margin-bottom: 1rem;
+    user-select: none;
+}
+
+.filter-buttons .platforms-label {
+    display: flex;
+    margin-right: 0.5rem;
+    justify-content: center;
+    align-items: center;
+}
+
+.platform-filters {
+    display: flex;
+    flex-direction: row;
+}
+
+.platform-filters .filter {
+    margin-right: 0.5rem;
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.25rem;
+    background-color: var(--main-blue);
+    color: white;
+    box-shadow: var(--main-shadow);
+}
+
+.filter:hover {
+    cursor: pointer;
+    filter: brightness(0.9);
+    transform: scale(1.05);
+}
+
+.platform-filters .deselected {
+    filter: opacity(50%);
+}
+</style>
+
+<style>
+.platform-filtered {
+    display: none;
+}
+</style>
