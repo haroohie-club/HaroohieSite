0eb5368008f1f312e712e3d4baea68d58b475f29
William <will27528@gmail.com>
2022-10-17 11:43:54 +0100

    Add some meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,11 +19,67 @@ export default defineNuxtConfig({
         }
     },
 
+    // App config
+    app: {
+        head: {
+            charset: 'utf-16',
+            viewport: 'width=500, initial-scale=1',
+            title: 'Haroohie Translation Club',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Welcome to the Haroohie Translation Club! We\'re dedicated to translating games from the Haruhi Suzumiya series into English!'
+                },
+                {
+                    name: 'og:title',
+                    content: 'Haroohie Translation Club'
+                },
+                {
+                    name: 'og:description',
+                    content: 'Welcome to the Haroohie Translation Club! We\'re dedicated to translating games from the Haruhi Suzumiya series into English!'
+                },
+                {
+                    name: 'og:site_name',
+                    content: 'Haroohie Translation Club - Translating the Haruhi Suzumiya games into English!'
+                },
+                {
+                    name: 'og:url',
+                    content: 'https://haroohie.club/'
+                },
+                {
+                    name: 'og:type',
+                    content: 'product'
+                },
+                {
+                    name: 'twitter:card',
+                    content: 'summary'
+                },
+                {
+                    name: 'twitter:site',
+                    content: '@haroohie'
+                },
+                {
+                    name: 'twitter:title',
+                    content: 'Haroohie Translation Club'
+                },
+                {
+                    name: 'twitter:description',
+                    content: 'Welcome to the Haroohie Translation Club! We\'re dedicated to translating games from the Haruhi Suzumiya series into English!'
+                },
+                {
+                    name: 'twitter:image',
+                    content: '/images/icon.png'
+                },
+                {
+                    name: 'twitter:image:alt',
+                    content: 'Suzumiya Haruhi no Chokuretsu logo overlaid with the SOS Brigade Mysterique Sign emblem'
+                }
+            ],
+        }
+    },
+
     content: {
         // https://content.nuxtjs.org/api/configuration
-        markdown: {
-            anchorLinks: false
-        },
         highlight: {
             theme: 'github-light',
             preload: [
