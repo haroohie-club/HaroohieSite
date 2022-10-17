4089f34b8d83f599d32ac98788696fbe4366b509
William <will27528@gmail.com>
2022-10-17 10:55:59 +0100

    Add YouTube video component for blogs

---
+++ components/content/YoutubeVideo.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Youtube video iframe -->
+    <div class="youtube-video">
+        <iframe
+            id="ytplayer"
+            :src="'https://www.youtube.com/embed/' + videoId + '?origin=' + $route.params.origin"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        videoId: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
