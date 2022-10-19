f6f04f259299ecfe8faeec631a3c894d28bd090b
jonko0493 <jonko0493@protonmail.com>
2022-10-18 21:32:25 -0700

    Remove old meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,57 +24,7 @@ export default defineNuxtConfig({
         head: {
             charset: 'utf-16',
             viewport: 'width=device-width, initial-scale=1',
-            title: 'Haroohie Translation Club',
-            meta: [
-                {
-                    name: 'description',
-                    content: 'Welcome to the Haroohie Translation Club! We\'re dedicated to translating games from the Haruhi Suzumiya series into English!'
-                },
-                {
-                    name: 'og:title',
-                    content: 'Haroohie Translation Club - Haruhi Game Translations!'
-                },
-                {
-                    name: 'og:description',
-                    content: 'Welcome to the Haroohie Translation Club! We\'re dedicated to translating games from the Haruhi Suzumiya series into English!'
-                },
-                {
-                    name: 'og:site_name',
-                    content: 'Haroohie Translation Club - Translating the Haruhi Suzumiya games into English!'
-                },
-                {
-                    name: 'og:url',
-                    content: 'https://haroohie.club/'
-                },
-                {
-                    name: 'og:type',
-                    content: 'product'
-                },
-                {
-                    name: 'twitter:card',
-                    content: 'summary'
-                },
-                {
-                    name: 'twitter:site',
-                    content: '@haroohie'
-                },
-                {
-                    name: 'twitter:title',
-                    content: 'Haroohie Translation Club'
-                },
-                {
-                    name: 'twitter:description',
-                    content: 'Welcome to the Haroohie Translation Club! We\'re dedicated to translating games from the Haruhi Suzumiya series into English!'
-                },
-                {
-                    name: 'twitter:image',
-                    content: '/images/icon.png'
-                },
-                {
-                    name: 'twitter:image:alt',
-                    content: 'Suzumiya Haruhi no Chokuretsu logo overlaid with the SOS Brigade Mysterique Sign emblem'
-                }
-            ],
+            title: 'Haroohie Translation Club'
         }
     },
 
