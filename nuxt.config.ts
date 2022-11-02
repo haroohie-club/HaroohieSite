ddfc8a441d7862fde3fdbaa75f8c2d5ff3674b0b
jonko0493 <jonko0493@protonmail.com>
2022-11-01 21:34:08 -0700

    ARM highlighting, images for blog post 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import armTMLanguage from './tmLanguages/arm.tmLanguage.json'
+
 // Nuxt config file (https://v3.nuxtjs.org/api/configuration/nuxt.config)
 export default defineNuxtConfig({
     // Modules
@@ -40,7 +42,13 @@ export default defineNuxtConfig({
                 'c',
                 'cpp',
                 'csharp',
-                'python'
+                'python',
+                {
+                    id: 'asm',
+                    scopeName: 'source.arm',
+                    aliases: [],
+                    grammar: armTMLanguage
+                }
             ]
         }
     }
