6f1c1df20d9e95dfd080509fa46ea775174e565e
William278 <will27528@gmail.com>
2024-01-25 01:08:24 +0000

    shijiki fixup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from 'fs'
+import { join } from 'path'
 
 // Nuxt config file (https://nuxt.com/docs/getting-started/configuration)
 export default {
@@ -45,10 +46,8 @@ export default {
                 'csharp',
                 'python',
                 {
-                    id: "arm",
-                    scopeName: 'source.arm',
-                    grammar: JSON.parse(readFileSync("./tmLanguages/arm.tmLanguage.json")),
-                    aliases: [],
+                    name: 'arm',
+                    ...JSON.parse(readFileSync('./tmLanguages/arm.tmLanguage.json', 'utf8'))
                 }
             ]
         }
