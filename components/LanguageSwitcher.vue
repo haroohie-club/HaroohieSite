0c68ba62de8049d409e7a799bd0bf8ec2450f8be
jonko0493 <jonko0493@protonmail.com>
2024-12-09 22:59:14 -0800

    Fixup statically generated locale change

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -3,7 +3,7 @@
         <label class="shadow" for="locale-select">
             <Icon name="fa6-solid:language" />
         </label>
-        <select class="shadow" id="locale-select" :value="locale" v-model="$i18n.locale" v-on:change="switchLocale(this.value)">
+        <select class="shadow" id="locale-select" :value="locale" v-model="$i18n.locale" v-on:change="switchLocale()">
             <option v-for="l in availableLocales" :value="l">
                 <span>{{ getLanguageName(l) }}</span>
                 <span class="lozenge" v-if="locale == l">&nbsp;&#x23F7;</span>
@@ -19,13 +19,13 @@ const { locale } = useI18n({
 })
 const localePath = useLocalePath()
 const switchLocalePath = useSwitchLocalePath()
-function switchLocale(l) {
-    window.location.href = switchLocalePath(l)
-}
 function getLanguageName(languageCode) {
     const nameGenerator = new Intl.DisplayNames(languageCode, { type: 'language' });
     const displayName = nameGenerator.of(languageCode);
     return displayName[0].toUpperCase() + displayName.substring(1);
+}
+const switchLocale = async () => {
+    await navigateTo(switchLocalePath(document.getElementById('locale-select').value))
 }
 </script>
 
